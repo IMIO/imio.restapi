230220765dd8618887de804fe3e59f583f8635f4
Martin Peeters <martin.peeters@affinitic.be>
2018-12-03 17:09:58 +0100

    Initial commit

---
+++ src/imio/restapi/testing.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
+from plone.app.testing import applyProfile
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneSandboxLayer
+from plone.testing import z2
+
+import imio.restapi
+
+
+class ImioRestapiLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load any other ZCML that is required for your tests.
+        # The z3c.autoinclude feature is disabled in the Plone fixture base
+        # layer.
+        import plone.app.dexterity
+        self.loadZCML(package=plone.app.dexterity)
+        self.loadZCML(package=imio.restapi)
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'imio.restapi:default')
+
+
+IMIO_RESTAPI_FIXTURE = ImioRestapiLayer()
+
+
+IMIO_RESTAPI_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(IMIO_RESTAPI_FIXTURE,),
+    name='ImioRestapiLayer:IntegrationTesting',
+)
+
+
+IMIO_RESTAPI_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(IMIO_RESTAPI_FIXTURE,),
+    name='ImioRestapiLayer:FunctionalTesting',
+)
+
+
+IMIO_RESTAPI_ACCEPTANCE_TESTING = FunctionalTesting(
+    bases=(
+        IMIO_RESTAPI_FIXTURE,
+        REMOTE_LIBRARY_BUNDLE_FIXTURE,
+        z2.ZSERVER_FIXTURE,
+    ),
+    name='ImioRestapiLayer:AcceptanceTesting',
+)
