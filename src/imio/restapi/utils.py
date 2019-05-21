c73048ac4a58d150912ebcd2cf2dada2e31a12e1
Martin Peeters <martin.peeters@affinitic.be>
2019-05-21 16:49:01 +0200

    Add global functions

---
+++ src/imio/restapi/utils.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import requests
+import time
+import os
+
+
+def get_ws_url():
+    """ Return the webservice url defined in instance config """
+    return os.getenv("WS_URL")
+
+
+def get_client_id():
+    """ Return the client_id defined in instance config """
+    return os.getenv("CLIENT_ID")
+
+
+def get_application_id():
+    """ Return the application_id defined in instance config """
+    return os.getenv("APPLICATION_ID")
+
+
+def ws_synchronous_request(method, *args, **kwargs):
+    me = {'GET': requests.get, 'POST': requests.post}.get(method)
+    r = me(*args, **kwargs)
+    if r.status_code == 200:
+        kwargs['json'] = r.json()
+        result = False
+        counter = 0
+        while result == False:
+            r = requests.get(*args, **kwargs)
+            print('request {0}'.format(counter))
+            if r.status_code == 200:
+                result = True
+            else:
+                if counter >= 500:
+                    return r
+                counter += 1
+                time.sleep(0.01)
+        return r
