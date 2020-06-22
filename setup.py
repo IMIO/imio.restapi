c5f9cbb5cc6176c3716377b46078135d3d386369
Martin Peeters <martin.peeters@affinitic.be>
2020-06-22 14:34:06 +0200

    Back to development: 1.0a5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="imio.restapi",
-    version="1.0a4",
+    version="1.0a5.dev0",
     description="Extended rest api service for IMIO usecases",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
