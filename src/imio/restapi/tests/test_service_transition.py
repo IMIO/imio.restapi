83ef35c397a0f02a62fa4ab5d64981ceb57b2a48
Piret Valentin <valentin.piret@affinitic.be>
2021-09-13 14:01:23 +0200

    Update test for transistion and update

--- src/imio/restapi/tests/test_service_transition.py
+++ src/imio/restapi/tests/test_service_transition.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from base64 import b64encode
 from DateTime import DateTime
 from imio.restapi.testing import IMIO_RESTAPI_WORKFLOWS_INTEGRATION_TESTING
 from plone.app.testing import login
@@ -11,9 +10,9 @@ from plone.app.testing import TEST_USER_NAME
 from plone.app.testing import TEST_USER_PASSWORD
 from Products.CMFCore.utils import getToolByName
 from unittest import TestCase
-from zExceptions import NotFound
-from zope.event import notify
-from ZPublisher.pubevents import PubStart
+
+import requests
+import transaction
 
 
 class TestWorkflowTransition(TestCase):
@@ -23,74 +22,108 @@ class TestWorkflowTransition(TestCase):
     def setUp(self):
         self.portal = self.layer["portal"]
         self.request = self.layer["request"]
+        self.portal_url = self.portal.absolute_url()
         self.wftool = getToolByName(self.portal, "portal_workflow")
         login(self.portal, SITE_OWNER_NAME)
         self.portal.invokeFactory("Document", id="doc1")
+        self.folder = self.portal[
+            self.portal.invokeFactory("Folder", id="folder", title="Test")
+        ]
+        self.subfolder = self.folder[
+            self.folder.invokeFactory("Folder", id="subfolder")
+        ]
+        transaction.commit()
 
-    def traverse(
-        self, path="/plone", accept="application/json", method="POST", auth=None
-    ):
-        request = self.layer["request"]
-        request.environ["PATH_INFO"] = path
-        request.environ["PATH_TRANSLATED"] = path
-        request.environ["HTTP_ACCEPT"] = accept
-        request.environ["REQUEST_METHOD"] = method
-        if auth is None:
-            auth = "%s:%s" % (SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
-        request._auth = "Basic %s" % b64encode(auth.encode("utf8")).decode("utf8")
-        notify(PubStart(request))
-        return request.traverse(path)
+    def tearDown(self):
+        login(self.portal, SITE_OWNER_NAME)
+        self.portal.manage_delObjects(["doc1"])
+        self.portal.manage_delObjects(["folder"])
+        transaction.commit()
 
     def test_transition_action_succeeds(self):
         uid = self.portal.doc1.UID()
-        service = self.traverse("/plone/@wf/{0}/publish".format(uid))
-        service.reply()
+        endpoint_url = "{0}/@wf/{1}/publish".format(self.portal_url, uid)
+        requests.post(
+            endpoint_url,
+            headers={"Accept": "application/json"},
+            auth=(SITE_OWNER_NAME, SITE_OWNER_PASSWORD),
+            json={},
+        )
+        transaction.commit()
         self.assertEqual(
             u"published", self.wftool.getInfoFor(self.portal.doc1, u"review_state")
         )
 
     def test_transition_action_succeeds_changes_effective(self):
-        doc1 = self.portal.doc1
-        uid = doc1.UID()
-        self.assertEqual(doc1.effective_date, None)
+        uid = self.portal.doc1.UID()
+        endpoint_url = "{0}/@wf/{1}/publish".format(self.portal_url, uid)
+        self.assertEqual(self.portal.doc1.effective_date, None)
         now = DateTime()
-        service = self.traverse("/plone/@wf/{0}/publish".format(uid))
-        service.reply()
-        self.assertTrue(isinstance(doc1.effective_date, DateTime))
-        self.assertTrue(doc1.effective_date >= now)
+        requests.post(
+            endpoint_url,
+            headers={"Accept": "application/json"},
+            auth=(SITE_OWNER_NAME, SITE_OWNER_PASSWORD),
+            json={},
+        )
+        transaction.commit()
+        self.assertTrue(isinstance(self.portal.doc1.effective_date, DateTime))
+        self.assertTrue(self.portal.doc1.effective_date >= now)
 
     def test_calling_workflow_with_additional_path_segments_results_in_404(self):
         uid = self.portal.doc1.UID()
-        with self.assertRaises(NotFound):
-            self.traverse("/plone/doc1/@wf/{0}/publish/test".format(uid))
+        endpoint_url = "{0}/@wf/{1}/publish/test".format(self.portal_url, uid)
+        response = requests.post(
+            endpoint_url,
+            headers={"Accept": "application/json"},
+            auth=(SITE_OWNER_NAME, SITE_OWNER_PASSWORD),
+            json={},
+        )
+        transaction.commit()
+        self.assertEqual(404, response.status_code)
 
     def test_transition_including_children(self):
-        folder = self.portal[self.portal.invokeFactory("Folder", id="folder")]
-        subfolder = folder[folder.invokeFactory("Folder", id="subfolder")]
-        uid = folder.UID()
-        self.request["BODY"] = '{"include_children": true}'
-        service = self.traverse("/plone/@wf/{0}/publish".format(uid))
-        service.reply()
-        self.assertEqual(200, self.request.response.getStatus())
-        self.assertEqual(u"published", self.wftool.getInfoFor(folder, u"review_state"))
+        transaction.commit()
+        uid = self.folder.UID()
+        endpoint_url = "{0}/@wf/{1}/publish".format(self.portal_url, uid)
+        response = requests.post(
+            endpoint_url,
+            headers={"Accept": "application/json"},
+            auth=(SITE_OWNER_NAME, SITE_OWNER_PASSWORD),
+            json={"include_children": "true"},
+        )
+        transaction.commit()
+        self.assertEqual(200, response.status_code)
         self.assertEqual(
-            u"published", self.wftool.getInfoFor(subfolder, u"review_state")
+            u"published", self.wftool.getInfoFor(self.folder, u"review_state")
+        )
+        self.assertEqual(
+            u"published", self.wftool.getInfoFor(self.subfolder, u"review_state")
         )
 
     def test_transition_with_effective_date(self):
-        self.request["BODY"] = '{"effective": "2018-06-24T09:17:02"}'
         uid = self.portal.doc1.UID()
-        service = self.traverse("/plone/@wf/{0}/publish".format(uid))
-        service.reply()
+        endpoint_url = "{0}/@wf/{1}/publish".format(self.portal_url, uid)
+        requests.post(
+            endpoint_url,
+            headers={"Accept": "application/json"},
+            auth=(SITE_OWNER_NAME, SITE_OWNER_PASSWORD),
+            json={"effective": "2018-06-24T09:17:02"},
+        )
+        transaction.commit()
         self.assertEqual(
             "2018-06-24T09:17:00+00:00", self.portal.doc1.effective().ISO8601()
         )
 
     def test_transition_with_expiration_date(self):
-        self.request["BODY"] = '{"expires": "2019-06-20T18:00:00"}'
         uid = self.portal.doc1.UID()
-        service = self.traverse("/plone/@wf/{0}/publish".format(uid))
-        service.reply()
+        endpoint_url = "{0}/@wf/{1}/publish".format(self.portal_url, uid)
+        requests.post(
+            endpoint_url,
+            headers={"Accept": "application/json"},
+            auth=(SITE_OWNER_NAME, SITE_OWNER_PASSWORD),
+            json={"expires": "2019-06-20T18:00:00"},
+        )
+        transaction.commit()
         self.assertEqual(
             "2019-06-20T18:00:00+00:00", self.portal.doc1.expires().ISO8601()
         )
@@ -98,16 +131,21 @@ class TestWorkflowTransition(TestCase):
     def test_transition_with_no_access_to_review_history_in_target_state(self):
         self.wftool.setChainForPortalTypes(["Folder"], "restriction_workflow")
         folder = self.portal[
-            self.portal.invokeFactory("Folder", id="folder", title="Test")
+            self.portal.invokeFactory("Folder", id="folder_test", title="Test")
         ]
+        transaction.commit()
         uid = folder.UID()
         setRoles(
             self.portal, TEST_USER_ID, ["Contributor", "Editor", "Member", "Reviewer"]
         )
         login(self.portal, TEST_USER_NAME)
-
-        auth = "%s:%s" % (TEST_USER_NAME, TEST_USER_PASSWORD)
-        service = self.traverse("/plone/@wf/{0}/restrict".format(uid), auth=auth)
-        service.reply()
-        self.assertEqual(200, self.request.response.getStatus())
+        endpoint_url = "{0}/@wf/{1}/restrict".format(self.portal_url, uid)
+        response = requests.post(
+            endpoint_url,
+            headers={"Accept": "application/json"},
+            auth=(TEST_USER_NAME, TEST_USER_PASSWORD),
+            json={},
+        )
+        transaction.commit()
+        self.assertEqual(200, response.status_code)
         self.assertEqual(u"restricted", self.wftool.getInfoFor(folder, u"review_state"))
--- src/imio/restapi/tests/test_service_update.py
+++ src/imio/restapi/tests/test_service_update.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-from base64 import b64encode
 from imio.restapi.testing import IMIO_RESTAPI_DX_FUNCTIONAL_TESTING
 from plone.app.testing import login
 from plone.app.testing import setRoles
 from plone.app.testing import SITE_OWNER_NAME
 from plone.app.testing import SITE_OWNER_PASSWORD
 from plone.app.testing import TEST_USER_ID
+from plone.app.testing import TEST_USER_NAME
+from plone.app.testing import TEST_USER_PASSWORD
 from Products.CMFCore.utils import getToolByName
-from zope.event import notify
-from ZPublisher.pubevents import PubStart
 
+import requests
 import transaction
 import unittest
 
@@ -21,6 +21,7 @@ class TestContentPatch(unittest.TestCase):
         self.app = self.layer["app"]
         self.portal = self.layer["portal"]
         self.request = self.layer["request"]
+        self.portal_url = self.portal.absolute_url()
         setRoles(self.portal, TEST_USER_ID, ["Member"])
         login(self.portal, SITE_OWNER_NAME)
         self.portal.invokeFactory(
@@ -30,37 +31,48 @@ class TestContentPatch(unittest.TestCase):
         wftool.doActionFor(self.portal.doc1, "publish")
         transaction.commit()
 
-    def traverse(
-        self, path="/plone", accept="application/json", method="PATCH", auth=None
-    ):
-        request = self.layer["request"]
-        request.environ["PATH_INFO"] = path
-        request.environ["PATH_TRANSLATED"] = path
-        request.environ["HTTP_ACCEPT"] = accept
-        request.environ["REQUEST_METHOD"] = method
-        if auth is None:
-            auth = "%s:%s" % (SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
-        request._auth = "Basic %s" % b64encode(auth.encode("utf8")).decode("utf8")
-        notify(PubStart(request))
-        return request.traverse(path)
+    def tearDown(self):
+        login(self.portal, SITE_OWNER_NAME)
+        self.portal.manage_delObjects(["doc1"])
+        transaction.commit()
 
     def test_patch_document(self):
         self.request["BODY"] = '{"title": "Patched Document"}'
+
         uid = self.portal.doc1.UID()
-        service = self.traverse("/plone/@content/{0}".format(uid))
-        transaction.begin()
-        service.reply()
+        endpoint_url = "{0}/@content/{1}".format(self.portal_url, uid)
+        response = requests.patch(
+            endpoint_url,
+            headers={"Accept": "application/json"},
+            auth=(SITE_OWNER_NAME, SITE_OWNER_PASSWORD),
+            json={"title": "Patched Document"},
+        )
         transaction.commit()
-        self.assertEqual(204, self.request.response.getStatus())
+        self.assertEqual(204, response.status_code)
         self.assertEqual("Patched Document", self.portal.doc1.Title())
 
     def test_patch_document_will_delete_value_with_null(self):
         self.assertEqual(self.portal.doc1.description, "Some Description")
-        self.request["BODY"] = '{"description": null}'
         uid = self.portal.doc1.UID()
-        service = self.traverse("/plone/@content/{0}".format(uid))
-        transaction.begin()
-        service.reply()
+        endpoint_url = "{0}/@content/{1}".format(self.portal_url, uid)
+        response = requests.patch(
+            endpoint_url,
+            headers={"Accept": "application/json"},
+            auth=(SITE_OWNER_NAME, SITE_OWNER_PASSWORD),
+            json={"description": ""},
+        )
         transaction.commit()
-        self.assertEqual(204, self.request.response.getStatus())
+        self.assertEqual(204, response.status_code)
         self.assertEqual(u"", self.portal.doc1.description)
+
+    def test_patch_document_unauthorized(self):
+        uid = self.portal.doc1.UID()
+        endpoint_url = "{0}/@content/{1}".format(self.portal_url, uid)
+        response = requests.patch(
+            endpoint_url,
+            headers={"Accept": "application/json"},
+            auth=(TEST_USER_NAME, TEST_USER_PASSWORD),
+            json={"description": ""},
+        )
+        transaction.commit()
+        self.assertEqual(401, response.status_code)
