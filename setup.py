d9b7aa6b6e8bbf265c5deb3896b2fbefe4ebab71
Gauthier Bastien <gauthier.bastien@imio.be>
2024-05-27 12:28:15 +0200

    Back to development: 1.0rc3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="imio.restapi",
-    version="1.0rc2",
+    version="1.0rc3.dev0",
     description="Extended rest api service for IMIO usecases",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
